b61e4ae3a76053ab5429aadcd4d1274f32022d48
bx5a <bx5a.develop@gmail.com>
2016-03-23 10:39:02 +0100

    Add action to search button

--- app/src/main/java/com/bx5a/minstrel/MainActivity.java
+++ app/src/main/java/com/bx5a/minstrel/MainActivity.java
@@ -1,10 +1,12 @@
 package com.bx5a.minstrel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,5 +31,21 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuMain_search:
+                startSearchView();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void startSearchView() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
 }
