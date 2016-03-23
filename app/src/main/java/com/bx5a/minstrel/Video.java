989194b7526972d76df5b702f0fd50b74ffacb7f
bx5a <bx5a.develop@gmail.com>
2016-03-23 16:17:27 +0100

    Add youtube data api and empty classes

---
+++ app/src/main/java/com/bx5a/minstrel/Video.java
@@ -0,0 +1,15 @@
+package com.bx5a.minstrel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by guillaume on 23/03/2016.
+ */
+public class Video {
+
+    public static ArrayList<Video> searchYoutube(String query) {
+
+        return null;
+    }
+}
+
