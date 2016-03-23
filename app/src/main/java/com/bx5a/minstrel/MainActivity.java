e635bc1a72d34fdad6c38aa94d86367384c8fc10
bx5a <bx5a.develop@gmail.com>
2016-03-23 10:30:18 +0100

    Add dummy side panel

--- app/src/main/java/com/bx5a/minstrel/MainActivity.java
+++ app/src/main/java/com/bx5a/minstrel/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Fill side panel
+        ListView sidePanel = (ListView)findViewById(R.id.activityMain_sidePanel);
+        String[] sidePanelItems = getResources().getStringArray(R.array.sidePanel_items);
+        sidePanel.setAdapter(
+                new ArrayAdapter<String>(this, R.layout.listitem_drawer, sidePanelItems));
     }
 
     @Override
