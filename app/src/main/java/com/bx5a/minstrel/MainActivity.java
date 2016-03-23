d64b45cb32d7018299bce987e573ee8a1fd13504
bx5a <bx5a.develop@gmail.com>
2016-03-23 09:32:49 +0100

    Initial commit

---
+++ app/src/main/java/com/bx5a/minstrel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bx5a.minstrel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
