f28a3afbe8b42d39026ef6a0b697e3a8d213198d
bx5a <bx5a.develop@gmail.com>
2016-04-12 17:48:23 +0200

    Make the video player a popup

--- app/src/main/java/com/bx5a/minstrel/MainActivity.java
+++ app/src/main/java/com/bx5a/minstrel/MainActivity.java
@@ -3,12 +3,18 @@ package com.bx5a.minstrel;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import com.bx5a.minstrel.youtube.DeveloperKey;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -17,23 +23,39 @@ import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PopupWindow youtubeVideo;
+    private View placeholder;
+    private View popupView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Fill side panel
-        /*ListView sidePanel = (ListView) findViewById(R.id.activityMain_sidePanel);
-        String[] sidePanelItems = getResources().getStringArray(R.array.sidePanel_items);
-        sidePanel.setAdapter(
-                new ArrayAdapter<String>(this, R.layout.listitem_drawer, sidePanelItems));*/
+        placeholder = findViewById(R.id.activityMain_videoPlaceholder);
 
-        // fragments
+        // init video popup
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupView = layoutInflater.inflate(R.layout.popup_youtubevideo, null);
+        youtubeVideo = new PopupWindow(
+                popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        // init youtube using that popup
         YouTubePlayerSupportFragment fragment = (YouTubePlayerSupportFragment) getSupportFragmentManager().findFragmentById(R.id.youtube_fragment);
         fragment.initialize(DeveloperKey.DEVELOPER_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 com.bx5a.minstrel.youtube.Player.getInstance().setYoutubePlayer(youTubePlayer);
+
+                // pop on the top right
+                DisplayMetrics metrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                Integer activityWidth =  metrics.widthPixels;
+                popupView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+                Integer popUpWidth = popupView.getMeasuredWidth();
+                youtubeVideo.showAsDropDown(placeholder,  activityWidth - popUpWidth, 0);
             }
 
             @Override
@@ -61,7 +83,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void startSearchView() {
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
+    private void startSearchView() {
+        // TODO: pop side panel
     }
 }
