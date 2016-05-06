baf2b5397a981249f7ea29a3cea7efcf6f2f3c96
bx5a <bx5a.develop@gmail.com>
2016-05-06 10:40:47 +0200

    Fix first gui test

--- app/src/androidTest/java/com/bx5a/minstrel/ElapsedTimeIdlingResource.java
+++ app/src/androidTest/java/com/bx5a/minstrel/ElapsedTimeIdlingResource.java
@@ -1,7 +1,36 @@
 package com.bx5a.minstrel;
 
-/**
- * Created by guitou on 06/05/16.
- */
-public class ElapsedTimeIdlingResource {
+// from https://github.com/chiuki/espresso-samples/blob/a70a343862a4199804f6fdffd9173cd453bb8d11/idling-resource-elapsed-time/app/src/androidTest/java/com/sqisland/espresso/idling_resource/elapsed_time/ElapsedTimeIdlingResource.java
+
+import android.support.test.espresso.IdlingResource;
+
+public class ElapsedTimeIdlingResource implements IdlingResource {
+    private final long startTime;
+    private final long waitingTime;
+    private ResourceCallback resourceCallback;
+
+    public ElapsedTimeIdlingResource(long waitingTime) {
+        this.startTime = System.currentTimeMillis();
+        this.waitingTime = waitingTime;
+    }
+
+    @Override
+    public String getName() {
+        return ElapsedTimeIdlingResource.class.getName() + ":" + waitingTime;
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        long elapsed = System.currentTimeMillis() - startTime;
+        boolean idle = (elapsed >= waitingTime);
+        if (idle) {
+            resourceCallback.onTransitionToIdle();
+        }
+        return idle;
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+        this.resourceCallback = resourceCallback;
+    }
 }
--- app/src/androidTest/java/com/bx5a/minstrel/PlayerTest.java
+++ app/src/androidTest/java/com/bx5a/minstrel/PlayerTest.java
@@ -1,20 +1,29 @@
 package com.bx5a.minstrel;
 
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
+import android.widget.EditText;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -30,17 +39,38 @@ public class PlayerTest {
         searchKeyword = "red hot";
     }
 
+    @After
+    public void destroy() {
+
+    }
+
     @Test
     public void enqueue_sameActivity() {
-        Log.i("enqueue_sameActivity", "Test started");
+        IdlingResource timeIdlingResource = new ElapsedTimeIdlingResource(5000);
+
         // pop side bar
         onView(withId(R.id.menuMain_search)).perform(click());
-        Log.i("enqueue_sameActivity", "Side panel opened");
         // open search
         onView(withId(R.id.activityMain_searchButton)).perform(click());
-        Log.i("enqueue_sameActivity", "Search fragment opened");
         // type keyword
-        onView(withId(R.id.viewSearch_search)).perform(typeText(searchKeyword), closeSoftKeyboard());
-        Log.i("enqueue_sameActivity", "Keyword entered");
+        onView(isAssignableFrom(EditText.class)).perform(typeText(searchKeyword));
+        // fix: force wait for search to be completed
+        Espresso.registerIdlingResources(timeIdlingResource);
+
+        // fix: make the soft keyboard disappear since closeSoftKeyboard doesn't work
+        pressBack();
+        Espresso.unregisterIdlingResources(timeIdlingResource);
+
+        // click on first and second item
+        onData(anything()).inAdapterView(withId(R.id.viewSearch_resultList)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.viewSearch_resultList)).atPosition(1).perform(click());
+
+        // fix again: Can't seem to access menu
+        pressBack();
+
+        onView(withId(R.id.menuMain_search)).perform(click());
+        onView(withId(R.id.activityMain_playlistButton)).perform(click());
+        // remove currently playing song
+        onData(anything()).inAdapterView(withId(R.id.fragmentPlaylist_list)).atPosition(0).perform(longClick());
     }
 }
